bdfd0f15068ca68ae78d6be3cba41d9a1fce7ac5
unknown <gohars1@mcmaster.ca>
2021-02-06 20:47:33 -0500

    Pulling data from fitbit

--- shopbit/fitbitdata/urls.py
+++ shopbit/fitbitdata/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/<str:code>/<str:state>/', views.test_endpoint, name='test_endpoint')
 ]
--- shopbit/fitbitdata/views.py
+++ shopbit/fitbitdata/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import fitbit
+from fitbit.api import Fitbit
+from urllib.parse import urlparse
+import config
+import threading
+import webbrowser
+import requests
 
+
+
+CLIENT_ID = config.CLIENT_ID
+CLIENT_SECRET = config.CLIENT_SECRET
+redirect_uri='http://127.0.0.1:8000/'
+fitbit = Fitbit(CLIENT_ID, CLIENT_SECRET, redirect_uri, timeout=10)
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    url, _ = fitbit.client.authorize_token_url()
+    print(url)
+    threading.Timer(1, webbrowser.open, args=(url,)).start()
+
+    return HttpResponse("you made it" + url)
+
+def test_endpoint(request, code, state):
+    
+    # fitbit.access_token = code
+    access = fitbit.client.fetch_access_token(code)
+    print(request.content_type)
+  #  print(fitbit.activities())
+    # bp = fitbit.bp()
+    return HttpResponse("hello")
